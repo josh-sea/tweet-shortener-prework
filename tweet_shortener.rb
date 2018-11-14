e8874a6fc10bbdf106249157eb6a55252edc3ebe
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 06:08:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ def word_substituter(tweet)
   tweet_array = tweet.split(" ")
   for i in 0...tweet_array.length 
     if dictionary(i) != nil 
-      tweet_array(i) = dictionary(i)
+      tweet_array(i) = dictionary(tweet_array(i))
     end
   end
 end
