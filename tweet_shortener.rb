0f20b678c07297fe978272fb3e81f0c69d0f03fa
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 05:58:22 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,13 @@
 # Write your code here.
 def dictionary
   short_hash = {
-"hello" => 'hi',
-"to, two, too" => '2', 
-"for, four" => '4',
-'be' => 'b',
-'you' => 'u',
-"at" => "@", 
-"and" => "&",
-}
+  "hello" => 'hi',
+  "to, two, too" => '2', 
+  "for, four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@", 
+  "and" => "&"
+  }
+  short_hash
 end
