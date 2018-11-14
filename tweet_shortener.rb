430c80fe28487171ed51953615c88963b45f683d
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 06:15:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,11 +18,11 @@ end
 def word_substituter(tweet)
   tweet_array = tweet.split(" ")
   short_tweet = []
-  for i in 0...tweet_array.length 
-    if dictionary(i) != nil 
-      short_tweet(i) = dictionary(tweet_array(i))
+  tweet_array.each do |word|
+    if dictionary(word) == nil 
+      short_tweet << word
     else
-      short_tweet(i) = tweet_array(i)
+      short_tweet << dictionary(word)
     end
   end
   short_tweet.join(" ")
