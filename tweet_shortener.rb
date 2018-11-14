7ef1e407c21b93c88d575a4b7364abe6557e2dcc
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 06:04:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,3 +16,18 @@ def dictionary(key)
 end
 
 
+def word_substituter(key)
+  short_hash = {
+  "hello" => 'hi',
+  "to" => '2',
+  "two" => '2',
+  "too" => '2', 
+  "for" => '4',
+  "four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@", 
+  "and" => "&"
+  }
+  return short_hash[key]
+end
